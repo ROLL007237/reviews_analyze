eab204fde464eb49b8d62d3fd8df23a9e632e137
ROLL007237 <orange-ez@mail.ru>
2026-09-08 15:10:31 +0300

    AVE MARIA

--- ai-processing-service/docker-entrypoint.sh
+++ ai-processing-service/docker-entrypoint.sh
@@ -32,4 +32,6 @@ echo "Migrations completed."
 
 echo "Starting application..."
 
+uvicorn app.main:app --host 0.0.0.0 --port 8000
+
 exec "$@"
--- ingestion-service/docker-entrypoint.sh
+++ ingestion-service/docker-entrypoint.sh
@@ -32,4 +32,7 @@ echo "Migrations completed."
 
 echo "Starting application..."
 
+uvicorn app.main:app --host 0.0.0.0 --port 8000
+
 exec "$@"
+
